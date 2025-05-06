1dc9bb831272a1cde6b19c2506ebdf18a0ca3a40
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-06 04:19:46 +0300

    setup update

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="rossmann_oltp",
     version="0.1",
     packages=find_packages(include=['rossmann_oltp.models.*', 
-                                    'rossmann_oltp.employee_role'])
+                                    'rossmann_oltp.employee_role']),
+    install_requires=[line.strip() for line in open("requirements.txt") if line.strip() and not line.startswith("#")],
 )
