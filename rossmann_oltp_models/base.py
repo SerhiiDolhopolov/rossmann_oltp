8f8a08242516fdc3601fa186d58e2ce6d419be0e
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-19 09:49:48 +0300

    fgf

--- rossmann_oltp_models/base.py
+++ rossmann_oltp_models/base.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+from sqlalchemy import event
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -15,3 +16,11 @@ class SyncBase(Base):
     last_updated_utc: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc).strftime(DATE_TIME_FORMAT),
                                                        index=True)
     is_deleted: Mapped[bool] = mapped_column(default=False, index=True)
+    
+def update_last_updated_utc(mapper, connection, target):
+    from datetime import datetime, timezone
+    from rossmann_oltp_models.config import DATE_TIME_FORMAT
+    target.last_updated_utc = datetime.now(timezone.utc).strftime(DATE_TIME_FORMAT)
+
+for cls in SyncBase.__subclasses__():
+    event.listen(cls, "before_update", update_last_updated_utc)
