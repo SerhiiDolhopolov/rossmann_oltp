ef5a4aff5744e5478ed6fe09f00ce849ed27255a
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-20 06:27:27 +0300

    product desc schema

--- rossmann_sync_schemas/__init__.py
+++ rossmann_sync_schemas/__init__.py
@@ -1,4 +1,5 @@
 from .employee_schema import EmployeeSchema
 from .category_schema import CategorySchema
+from .product_desc_schema import ProductDescSchema
 from .product_schema import ProductSchema
 from .shop_schema import ShopSchema
--- rossmann_sync_schemas/product_desc_schema.py
+++ rossmann_sync_schemas/product_desc_schema.py
@@ -1,13 +1,10 @@
-from pydantic import Field
-from rossmann_sync_schemas import ProductDescSchema
+from pydantic import BaseModel, Field
 
 
-class ProductSchema(ProductDescSchema):
+class ProductDescSchema(BaseModel):
     product_id: int
     name: str = Field(max_length=255)
     description: str | None = Field(max_length=2048)
     barcode: str = Field(max_length=12)
     category_id: int
-    price: float = Field(gt=0)
-    discount: float = Field(ge=0, le=1)
     is_deleted: bool
