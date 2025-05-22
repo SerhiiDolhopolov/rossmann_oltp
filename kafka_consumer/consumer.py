4ab7e5cd3be6e697d12e9b7ffb3244bccf90381b
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-22 03:58:13 +0300

    kafka consumer reconnect

--- kafka_consumer/consumer.py
+++ kafka_consumer/consumer.py
@@ -3,15 +3,18 @@ from confluent_kafka import Consumer, KafkaException, KafkaError
 from database.config import KAFKA_TOPIC_OLTP_UPDATE_PRODUCT_QUANTITY, KAFKA_HOST, KAFKA_PORT
 from database.services.sync_service import update_products_quantity
 from database.db import get_db
+import time
 
 
 def consume_messages(topic_handlers: dict[str, callable]):
-    consumer = Consumer({
-        'bootstrap.servers': f'{KAFKA_HOST}:{KAFKA_PORT}',
-        'group.id': 'rossmann-oltp-group',
-        'auto.offset.reset': 'latest'
-    })
+    def create_consumer():
+        return Consumer({
+            'bootstrap.servers': f'{KAFKA_HOST}:{KAFKA_PORT}',
+            'group.id': 'rossmann-oltp-group',
+            'auto.offset.reset': 'latest'
+        })
 
+    consumer = create_consumer()
     topics = list(topic_handlers.keys())
     consumer.subscribe(topics)
     print(f"Consumer started. Subscribed to topics: {topics}")
@@ -24,14 +27,34 @@ def consume_messages(topic_handlers: dict[str, callable]):
 
     try:
         while True:
-            msg = consumer.poll(1.0)
+            try:
+                msg = consumer.poll(1.0)
+            except KafkaException as e:
+                print(f"KafkaException: {e}. Reconnect at 5 seconds...")
+                consumer.close()
+                time.sleep(5)
+                consumer = create_consumer()
+                consumer.subscribe(topics)
+                continue
+            except Exception as e:
+                print(f"Message receiving error: {e}. Reconnect at 5 seconds...")
+                consumer.close()
+                time.sleep(5)
+                consumer = create_consumer()
+                consumer.subscribe(topics)
+                continue
+
             if msg is None:
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
                     print(f"Reached end of partition: {msg.topic()} [{msg.partition()}]")
                 elif msg.error():
-                    raise KafkaException(msg.error())
+                    print(f"Kafka error: {msg.error()}. Попытка переподключения через 5 секунд...")
+                    consumer.close()
+                    time.sleep(5)
+                    consumer = create_consumer()
+                    consumer.subscribe(topics)
                 continue
 
             topic = msg.topic()
