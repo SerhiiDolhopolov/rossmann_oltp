3a2a3d8eb42a14a17be93a235bbc4e5ef88c931a
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-26 21:33:11 +0300

    kafka consumer reload

--- kafka_consumer/main.py
+++ kafka_consumer/main.py
@@ -2,7 +2,7 @@ import json
 import time
 import logging
 
-from confluent_kafka import Consumer, KafkaException, KafkaError
+from confluent_kafka import Consumer, KafkaError
 from database.config import (
     KAFKA_TOPIC_OLTP_UPDATE_PRODUCT_QUANTITY,
     KAFKA_HOST,
@@ -15,18 +15,11 @@ from config import init_logging
 init_logging()
 logger = logging.getLogger(__name__)
 
-
+    
 def consume_messages(topic_handlers: dict[str, callable]):
-    def connect_consumer():
-        consumer = Consumer(
-            {
-                "bootstrap.servers": f"{KAFKA_HOST}:{KAFKA_PORT}",
-                "group.id": "rossmann-oltp-group",
-                "auto.offset.reset": "latest",
-                "logger": logging.getLogger("confluent_kafka"),
-            }
-        )
-        topics = list(topic_handlers.keys())
+    consumer = get_kafka_consumer()
+    topics = list(topic_handlers.keys())
+    try:
         consumer.subscribe(topics)
         logger.info("Consumer started. Subscribed to topics: %s", topics)
 
@@ -35,32 +28,21 @@ def consume_messages(topic_handlers: dict[str, callable]):
             consumer.poll(1.0)
             time.sleep(5)
         logger.info("Assigned partitions: %s", consumer.assignment())
-        return consumer
 
-    def reconnect_consumer(consumer) -> Consumer:
-        if consumer:
-            consumer.close()
-        time.sleep(5)
-        return connect_consumer()
-
-    consumer = connect_consumer()
-    try:
         while True:
-            try:
-                msg = consumer.poll(1.0)
-                if msg is None:
-                    continue
-                if msg.error():
-                    if msg.error().code() == KafkaError._PARTITION_EOF:
-                        logger.debug(
-                            "End of partition reached: %s [%s]",
-                            msg.topic(),
-                            msg.partition(),
-                        )
-                    else:
-                        logger.error("Kafka error: %s", msg.error())
-                        consumer = reconnect_consumer(consumer)
-                        continue
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    logger.debug(
+                        "End of partition reached: %s [%s]",
+                        msg.topic(),
+                        msg.partition(),
+                    )
+                else:
+                    logger.error("Kafka error: %s", msg.error())
+            else:
                 topic = msg.topic()
                 value = msg.value().decode("utf-8")
                 logger.debug("Topic: %s\nReceived message: %s", topic, value)
@@ -68,15 +50,22 @@ def consume_messages(topic_handlers: dict[str, callable]):
                     try:
                         topic_handlers[topic](value)
                     except Exception:
-                        logger.error(
-                            "Error processing message: %s", exc_info=True)
-            except KafkaException as e:
-                logger.error("Kafka exception: %s. Reconnect...", e)
-                consumer = reconnect_consumer(consumer)
+                        logger.error("Error processing message: %s", value, exc_info=True)
     finally:
         consumer.close()
 
 
+def get_kafka_consumer() -> Consumer:
+    return Consumer(
+        {
+            "bootstrap.servers": f"{KAFKA_HOST}:{KAFKA_PORT}",
+            "group.id": "rossmann-oltp-group",
+            "auto.offset.reset": "latest",
+            "logger": logging.getLogger("confluent_kafka"),
+        }
+    )
+    
+    
 def update_product_quantity_by_kafka(value: str):
     data = json.loads(value)
     db = next(get_db())
